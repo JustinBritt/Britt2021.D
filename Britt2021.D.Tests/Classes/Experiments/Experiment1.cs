b3b4f2dcfd9bd59661bc46c3a6e7ad560e3629c9
Justin Britt <britt@uwindsor.ca>
2022-04-30 02:27:20 -0400

    Added tests

---
+++ Britt2021.D.Tests/Classes/Experiments/Experiment1.cs
@@ -0,0 +1,50 @@
+﻿namespace Britt2021.D.Tests.Classes.Experiments
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2021.D.InterfacesAbstractFactories;
+    using Britt2021.D.Interfaces.Experiments;
+
+    [TestClass]
+    public sealed class Experiment1
+    {
+        private IExperiment1 CreateExperiment1()
+        {
+            IAbstractFactory abstractFactory = Britt2021.D.AbstractFactories.AbstractFactory.Create();
+
+            ICalculationsAbstractFactory calculationsAbstractFactory = abstractFactory.CreateCalculationsAbstractFactory();
+
+            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
+
+            IExperimentsAbstractFactory experimentsAbstractFactory = abstractFactory.CreateExperimentsAbstractFactory();
+
+            return experimentsAbstractFactory.CreateExperiment1Factory().Create(
+                calculationsAbstractFactory,
+                dependenciesAbstractFactory);
+        }
+
+        [DataTestMethod]
+        [DataRow(8)]
+        public void NumberClusters(
+            int numberClusters)
+        {
+            // Arrange
+            IExperiment1 experiment1 = this.CreateExperiment1();
+
+            // Act
+            ImmutableList<PositiveInt> clusters = experiment1.Clusters;
+
+            // Assert
+            Assert.AreEqual(
+                expected: numberClusters,
+                actual: clusters.Count);
+        }
+    }
+}
