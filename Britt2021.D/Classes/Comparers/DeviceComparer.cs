a5c1789de1b75c8b3d605df29d516883b97d495d
Justin Britt <britt@uwindsor.ca>
2023-06-09 02:45:35 -0400

    Added class

---
+++ Britt2021.D/Classes/Comparers/DeviceComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2021.D.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2021.D.Interfaces.Comparers;
+
+    internal sealed class DeviceComparer : IDeviceComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DeviceComparer()
+        {
+        }
+
+        public int Compare(
+            Device x,
+            Device y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
