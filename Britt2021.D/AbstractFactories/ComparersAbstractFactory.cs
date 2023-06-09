6e937afe8c2be5e626edd5efe7663e22c926c380
Justin Britt <britt@uwindsor.ca>
2023-06-08 20:42:00 -0400

    Added abstract factory

---
+++ Britt2021.D/AbstractFactories/ComparersAbstractFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2021.D.AbstractFactories
+{
+    using Britt2021.D.Factories.Comparers;
+    using Britt2021.D.InterfacesAbstractFactories;
+    using Britt2021.D.InterfacesFactories.Comparers;
+
+    internal sealed class ComparersAbstractFactory : IComparersAbstractFactory
+    {
+        public ComparersAbstractFactory()
+        {
+        }
+
+        public INullableValueintComparerFactory CreateNullableValueintComparerFactory()
+        {
+            INullableValueintComparerFactory factory = null;
+
+            try
+            {
+                factory = new NullableValueintComparerFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ Britt2021.D/InterfacesAbstractFactories/IComparersAbstractFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2021.D.InterfacesAbstractFactories
+{
+    using Britt2021.D.InterfacesFactories.Comparers;
+
+    public interface IComparersAbstractFactory
+    {
+        INullableValueintComparerFactory CreateNullableValueintComparerFactory();
+    }
+}
