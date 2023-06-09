30781dd4bcdd0597c0e26d7da41d693a428559d1
Justin Britt <britt@uwindsor.ca>
2023-06-09 02:59:25 -0400

    Added factory

---
+++ Britt2021.D/Factories/Comparers/DeviceComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2021.D.Factories.Comparers
+{
+    using Britt2021.D.Classes.Comparers;
+    using Britt2021.D.Interfaces.Comparers;
+    using Britt2021.D.InterfacesFactories.Comparers;
+
+    internal sealed class DeviceComparerFactory : IDeviceComparerFactory
+    {
+        public DeviceComparerFactory()
+        {
+        }
+
+        public IDeviceComparer Create()
+        {
+            IDeviceComparer instance = null;
+
+            try
+            {
+                instance = new DeviceComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2021.D/InterfacesFactories/Comparers/IDeviceComparerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2021.D.InterfacesFactories.Comparers
+{
+    using Britt2021.D.Interfaces.Comparers;
+
+    public interface IDeviceComparerFactory
+    {
+        IDeviceComparer Create();
+    }
+}
