42e08b5458a25567bb6c6fe60672d26b190390b6
Justin Britt <britt@uwindsor.ca>
2023-06-08 20:02:16 -0400

    Added class

---
+++ Britt2021.D/Interfaces/Comparers/INullableValueintComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace Britt2021.D.Interfaces.Comparers
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    public interface INullableValueintComparer : IComparer<INullableValue<int>>
+    {
+    }
+}
