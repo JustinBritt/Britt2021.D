830ff90b9c58a32aa199bcfb1e051e6397a83634
Justin Britt <britt@uwindsor.ca>
2023-06-26 15:40:50 -0400

    Added class

---
+++ Britt2021.D/Classes/Comparers/LocationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2021.D.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2021.D.Interfaces.Comparers;
+
+    internal sealed class LocationComparer : ILocationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LocationComparer()
+        {
+        }
+
+        public int Compare(
+            Location x,
+            Location y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
